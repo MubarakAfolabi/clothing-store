41c51afdb8500c715cf950a6a28cd48ac30518b6
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-08-18 13:33:22 +0100

    feat: design and implement header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <main>
+        <section className="h-screen">
+          <Image
+            src="/home-1.png"
+            fill
+            className="object-cover"
+            alt="Latest Collection"
+          />
+        </section>
+      </main>
     </div>
   );
 }
---
+++ components/layout/Header.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { ShoppingCart, Search, Menu } from "lucide-react";
+
+export default function Header() {
+  return (
+    <header className="text-black fixed top-0 left-0 right-0 z-10 p-5">
+      <div className="bg-[hsla(0,0%,0%,0.1)] w-full flex items-center justify-between pr-5 rounded-full shadow-[0_5px_4px_hsla(0,0%,0%,0.25)]">
+        <Image
+          src="/logo.png"
+          width="80"
+          height="80"
+          alt="logo image"
+          className="object-cover"
+        />
+        <div className="text-white flex items-center gap-4 cursor-pointer">
+          <div className="border border-solid rounded-full p-2">
+            <ShoppingCart size="20" />
+          </div>
+          <div className="border border-solid rounded-full p-2 cursor-pointer">
+            <Search size="20" />
+          </div>
+          <div className="text-[hsla(52,98%,53%,1)]">
+            <Menu size="30" strokeWidth="3" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
