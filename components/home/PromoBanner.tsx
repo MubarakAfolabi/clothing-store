6a2c33ef8cb190d4728fc3578047024000f18e2c
MubarakAfolabi <mubarakbolu150@gmail.com>
2026-08-19 00:21:21 +0100

    feat: improve promo banner component  responsive design for larger screens

--- components/home/PromoBanner.tsx
+++ components/home/PromoBanner.tsx
@@ -2,26 +2,28 @@ import { MoveRight } from "lucide-react";
 
 export default function PromoBanner() {
   return (
-    <section className="flex flex-col gap-8 px-4 py-10 border-solid border-t border-b border-t-[hsla(0,0%,100%,0.4)] border-b-[hsla(0,0%,100%,0.4)]">
-      <h2 className="text-5xl font-bold">
+    <section className="flex flex-col gap-8 md:gap-12 px-4 py-10 border-solid border-t border-b border-t-[hsla(0,0%,100%,0.4)] border-b-[hsla(0,0%,100%,0.4)]">
+      <h2 className="text-5xl md:text-8xl font-bold">
         UNLOCK <span className="text-4xl">10%</span> OFF
       </h2>
-      <p className="text-[hsla(0,0%,100%,0.8)]">
+      <p className="text-[hsla(0,0%,100%,0.8)] md:text-xl">
         Get 10% off your first order, plus early access to new drops and
         behind-the-scenes archives.
       </p>
 
-      <form className="flex flex-col gap-6">
-        <input
-          type="text"
-          placeholder="First Name"
-          className="border-b border-[hsla(0,0%,100%,0.4)] w-full p-4"
-        />
-        <input
-          type="email"
-          placeholder="Email Address"
-          className="border-b border-[hsla(0,0%,100%,0.4)] w-full p-4"
-        />
+      <form className="flex flex-col gap-6 md:gap-14">
+        <div className="flex flex-col md:flex-row gap-6 md:max-w-[80%]">
+          <input
+            type="text"
+            placeholder="First Name"
+            className="border-b border-[hsla(0,0%,100%,0.4)] w-full p-4"
+          />
+          <input
+            type="email"
+            placeholder="Email Address"
+            className="border-b border-[hsla(0,0%,100%,0.4)] w-full p-4"
+          />
+        </div>
 
         <button
           type="submit"
